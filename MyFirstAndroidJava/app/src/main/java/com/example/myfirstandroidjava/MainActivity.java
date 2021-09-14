340243d5dccb2b4408333aead5c6608509d15b96
K KATO <mochi3desu4@gmail.com>
2021-09-14 18:56:05 +0900

    Initial commit

---
+++ MyFirstAndroidJava/app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstandroidjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ↓プログラム記述箇所↓
+
+        String hello = "Hello, World!";
+        int intValue = 123456;
+        long longValue = 123456L;
+        double doubleValue = 12.3456;
+        float floatValue = 12.3456F;
+        boolean booleanValue = true;
+
+        Log.d("MainActivity", hello);
+        Log.d("MainActivity", "" + intValue);
+        Log.d("MainActivity", "" + longValue);
+        Log.d("MainActivity", "" + doubleValue);
+        Log.d("MainActivity", "" + floatValue);
+        Log.d("MainActivity", "" + booleanValue);
+
+        //  ↑プログラム記述ココまで↑
+
+    }
+}
