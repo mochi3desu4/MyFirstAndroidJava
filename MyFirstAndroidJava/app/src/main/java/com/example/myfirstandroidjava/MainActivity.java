fab9ca4225cfd78b2646774207889bfce353b6c0
K KATO <mochi3desu4@gmail.com>
2021-09-15 14:41:43 +0900

    Viewを操作（文字内容、色、太さ変更）

--- MyFirstAndroidJava/app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
+++ MyFirstAndroidJava/app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
@@ -4,6 +4,8 @@ import static java.lang.Integer.sum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -17,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
         //  レイアウトからTextViewを取り出す
         TextView hello = findViewById(R.id.texitview_hello);
-        //  TextViewにセットされている文字を抜き出す
-        String text = hello.getText().toString();
-        //  ログに出力する
-        Log.d("MainActivity", text);
+        //  画面に表示するデータを用意する
+        String text = "Hello,Android!";
+        //  TextViewにデータをセットする
+        hello.setText(text);
+        //  Text装飾
+        hello.setTextColor(Color.parseColor("#FF0000"));
+        hello.setTypeface(Typeface.DEFAULT_BOLD);
+
+        /*
+        TextViewの仕様
+        TextView | Android Developers
+        https://developer.android.com/reference/android/widget/TextView/
+        ※プログラム実行時はJava設定から上書きされる
+         */
 
     }
 
