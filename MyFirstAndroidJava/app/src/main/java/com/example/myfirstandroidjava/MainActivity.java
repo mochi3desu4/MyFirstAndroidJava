d4579dbb1b077fd0b2deaeed450210814e0b5e46
K KATO <mochi3desu4@gmail.com>
2021-09-16 15:26:12 +0900

    学習：入力欄とボタン配置、タップ時の処理、入力内容引き出し、ラベルに入力内容を上書き（エラー：activity_main.xmlのハードコーディングについて）

--- MyFirstAndroidJava/app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
+++ MyFirstAndroidJava/app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
@@ -8,31 +8,30 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.editText);
+                String text = editText.getText().toString();
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(text);
+            }
 
-        //  レイアウトからTextViewを取り出す
-        TextView hello = findViewById(R.id.texitview_hello);
-        //  画面に表示するデータを用意する
-        String text = "Hello,Android!";
-        //  TextViewにデータをセットする
-        hello.setText(text);
-        //  Text装飾
-        hello.setTextColor(Color.parseColor("#FF0000"));
-        hello.setTypeface(Typeface.DEFAULT_BOLD);
+        });
 
-        /*
-        TextViewの仕様
-        TextView | Android Developers
-        https://developer.android.com/reference/android/widget/TextView/
-        ※プログラム実行時はJava設定から上書きされる
-         */
 
     }
 
